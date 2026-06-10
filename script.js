ec9bfd95dc1e6a2964dbaf5088427d2f6089b422
Elton-Silva607 <elton.silva.five@gmail.com>
2026-06-09 23:16:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let primeiroNumero = Number (prompt ("digite o primeiro Numero:"))
-
-let segundoNumero = Number (prompt ("digite o segundo Numero:"))
-        
-let soma = (primeiroNumero + segundoNumero) 
-
-alert ("O numero somado é " + soma )
