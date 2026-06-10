c00ce118ce5eb6c6501b542af2397d89cb047c08
Elton-Silva607 <elton.silva.five@gmail.com>
2026-06-09 23:11:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let primeiroNumero = Number (prompt ("digite o primeiro Numero:"))
+
+let segundoNumero = Number (prompt ("digite o segundo Numero:"))
+        
+let soma = (primeiroNumero + segundoNumero) 
+
+alert ("O numero somado é " + soma )
